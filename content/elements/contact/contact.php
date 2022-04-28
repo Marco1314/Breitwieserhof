a17857ad71c6976d78a5fd509f41247813168b20
Marco1314 <MarcoHadzija@gmx.at>
2022-04-28 19:58:46 +0200

    contact

--- content/elements/contact/contact.php
+++ content/elements/contact/contact.php
@@ -4,9 +4,10 @@
     $Nachname = $_POST["Nachname"];
     $Email = $_POST["Email-Adresse"];
     $Betreff = $_POST["Betreff"];
+    $message = $_POST["Nachricht"];
 
     $to = "guenter.edelsbrunner@liwest.at";
-    $body = "Nachname: ".$Nachname." "."Vorname: ".$Vorname." "."Betreff: ".$Betreff. " "."Email-Adresse vom Absender: ".$Email;
+    $body = "Nachname: ".$Nachname." "."Vorname: ".$Vorname." "."Betreff: ".$Betreff. " "."Email-Adresse vom Absender: ".$Email."Nachricht".$message;
 
     mail($to, $Betreff, $body);
 ?>
